4cd8306482912ad72aa14338c7e62e832738f350
Nicolás Cárcamo <falconrandom@gmail.com>
2022-01-13 22:06:59 -0300

    sessions creada

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,18 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by_email(params[:email])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to root_path, notice: 'Loged in successfully'
+    else
+      flash.now[:notice] = 'Email o password inválido'
+      render action: :new
+    end
   end
 
   def destroy
+    session[:user_id] = nil
+    redirect_to root_url, notice: 'Signed out successfully'
   end
 end
